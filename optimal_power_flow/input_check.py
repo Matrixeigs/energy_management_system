4ef5d45e734cc46573ae4a33f5dfa72ffce9ad4d
Matrixeigs <matrixeigs@gmail.com>
2017-11-23 16:52:51 +0800

    Add input information check for the optimal power flow procedure

---
+++ optimal_power_flow/input_check.py
@@ -0,0 +1,7 @@
+# The input information check function for the optimal power flow
+# Two types of information will be checked, shown as follows.
+# 1) universal model, which contains the connection and topology information within the microgrid parks
+# 2) local model, which contains the local sources information
+
+# As the function will be triggered sequentially, only funcion is provided, other than class.
+
