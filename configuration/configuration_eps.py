efaccb9d1c90ca8deb2a99130050ce21dc74cebe
Matrixeigs <matrixeigs@gmail.com>
2017-11-30 16:38:25 +0800

    Add threshold value for calculation

---
+++ configuration/configuration_eps.py
@@ -0,0 +1,7 @@
+# The configuration files of eps values in calculation
+default_eps = \
+    {
+        "OPF": 1,
+        "ED": 1,  # The generation status, >0 means avalible, otherwise, unavaliable
+        "UC": 2,
+    }
