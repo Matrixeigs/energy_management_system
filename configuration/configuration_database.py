ce6913ed4a0f8d7ec50bd38e1197a0039814b5cd
Matrixeigs <matrixeigs@gmail.com>
2017-12-02 15:09:28 +0800

    Update history database information

--- configuration/configuration_database.py
+++ configuration/configuration_database.py
@@ -11,6 +11,15 @@ local_database = \
         "db_str" : 'mysql+pymysql://' + 'lems' + ':' + '3' + '@' + 'localhost' + '/' + 'local_ems'
     }
 
+local_load_database = \
+    {
+        "db_str" : 'mysql+pymysql://' + 'lems' + ':' + '3' + '@' + 'localhost' + '/' + 'load_profile'
+    }
+
+local_history_database = \
+    {
+        "db_str" : 'mysql+pymysql://' + 'lems' + ':' + '3' + '@' + 'localhost' + '/' + 'history_data'
+    }
 #IP address of local EMSs,
 local_ems_ip_address = \
     {
