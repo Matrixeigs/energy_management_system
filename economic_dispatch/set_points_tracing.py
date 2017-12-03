86c95b00b555d8915518b3757eeef2d5c321fbfa
Matrixeigs <matrixeigs@gmail.com>
2017-12-03 20:04:26 +0800

    Set points tracing for the economic dispatch

---
+++ economic_dispatch/set_points_tracing.py
@@ -0,0 +1,4 @@
+# Setting point tracing function for the economic dispatch
+# 1) query long2middle database
+# 2) update models of DG status, UG status, Battery SOC and line flows
+# 3) if any information is missing, the setting point tracing will not be triggered
