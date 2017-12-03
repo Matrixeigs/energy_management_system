1de3b2f7125ef5b9afd8c813268f3f2bbd3af2bf
Matrixeigs <matrixeigs@gmail.com>
2017-12-03 20:06:52 +0800

    Add set-points tracing function for optimal power flow

---
+++ optimal_power_flow/set_ponits_tracing.py
@@ -0,0 +1,4 @@
+# Setting point tracing function for the optimal power flow
+# 1) query middle2short database
+# 2) update models of DG status, UG status, Battery SOC and line flows
+# 3) if any information is missing, the setting point tracing will not be triggered
