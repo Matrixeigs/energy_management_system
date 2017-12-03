9c90dd367235a88b2a878f46e489cfd7b3ae5ffe
Matrixeigs <matrixeigs@gmail.com>
2017-12-03 19:56:15 +0800

    Add long2middle function for the long-term operation

---
+++ unit_commitment/long2middle.py
@@ -0,0 +1,5 @@
+# The long2middle function is provided to convert the long-term opeartion to middle-term operation.
+# This operation is mainly related to the database operation.
+
+def long2middle(*args):
+    Target_time = args[0] # Target time is the start time of scheduling in long-term operation
